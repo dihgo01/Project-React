38646cbe7667cc73c32f8584c9880690bed65056
dihgo01 <luka.poke9@gmail.com>
2020-02-28 23:19:56 -0300

    First commit

---
+++ src/router.js
@@ -0,0 +1,17 @@
+import React  from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+
+
+import Main from './page/Main/Main';
+import Repository from './page/Repository/Repository';
+
+export default function Routes() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact component={Main} />
+                <Route path="/repository" component={Repository} />
+            </Switch>
+        </BrowserRouter>
+    )
+}
